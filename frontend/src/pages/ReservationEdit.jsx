3883083fdef115850b8b04b507b3281d6e7c3086
Simon Schab <simonmschab@gmail.com>
2025-04-27 22:25:49 +0200

    Added new features to the reservation edit page.

--- frontend/src/pages/ReservationEdit.jsx
+++ frontend/src/pages/ReservationEdit.jsx
@@ -1,13 +1,14 @@
 import '../styles/ReservationEdit.css';
 import { reservationCookieName } from './Movies';
 import { useNavigate } from 'react-router-dom';
-import {deleteCookie, getCookie, setCookie} from '../Utilities/cookieUtils';
+import {getCookie} from '../Utilities/cookieUtils';
 import { useEffect, useState} from 'react';
 function ReservationEdit() {
     const navigate = useNavigate();
     const [cookieContent, setCookieContent] = useState([]);
     const decodedCookieContent = decodeURIComponent(getCookie(reservationCookieName));
     const [title, genre, durationInMinutes, format, screeningTime, selectedSeatsIds] = decodedCookieContent.split(';');
+    const [isDropdownActive, setIsDropdownActive] = useState(false);
     useEffect(() => {
         setCookieContent([title, genre, durationInMinutes, format, screeningTime, selectedSeatsIds.split('-')]);
     }, [title, genre, durationInMinutes, format, screeningTime, selectedSeatsIds]);
@@ -19,6 +20,9 @@ function ReservationEdit() {
             navigate('/');
         }
     }, [navigate]);
+    const toggleDropdown = () => {
+        setIsDropdownActive(!isDropdownActive);
+    };
     return (
         <div className="reservation-edit">
             {
@@ -40,6 +44,38 @@ function ReservationEdit() {
                     new Date(cookieContent[4]).toLocaleDateString('en-GB').replace(/\//g, '-')
                 }
             </h1>
+            <div className="reservation-edit-space-1"></div>
+            {
+                cookieContent[5]?.toString().split(',').map((seatId,index) => (
+                    <div className="box" id="reservation-edit-ticket-list-item" key={index}>
+                        <div className="reservation-edit-ticket-list-item-header">
+                            Row: {Math.floor(seatId / 16) + 1 + " | "}
+                            Seat: {(seatId % 16) + 1}
+                        </div>
+                        <div className="reservation-edit-space-3"></div>
+                        <div className="reservation-edit-ticket-list-item-content">
+                            Type:
+                            <div className={`dropdown ${isDropdownActive ? 'is-active' : ''}`}>
+                                <div className="dropdown-trigger">
+                                    <button className="button" id="reservation-edit-ticket-list-item-content-dropdown-button" onClick={toggleDropdown}>
+                                        <span className="reservation-edit-ticket-list-item-content-dropdown-button-label">sdf</span>
+                                        <i className="fas fa-angle-down"></i>
+                                    </button>
+                                </div>
+                                <div className="dropdown-menu">
+                                    <div className="dropdown-content">
+                                        <button className="dropdown-item">ljkhkljh</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="reservation-edit-ticket-list-item-content">
+                            Price:
+                        </div>
+                    </div>
+                ))
+            }
+            <div className="reservation-edit-space-2"></div>
         </div>
     );
 }
